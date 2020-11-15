bf5a9c9df1cd9d31fd2c9a0535432fd213c65d0a
Terry Worona <terryworona@gmail.com>
2020-11-14 20:21:04 -0800

    Cell

--- CalendarHeatmap/Classes/CalendarHeatmapCell.swift
+++ CalendarHeatmap/Classes/CalendarHeatmapCell.swift
@@ -10,6 +10,12 @@ import UIKit
 
 open class CalendarHeatmapCell: UICollectionViewCell {
     
+    var shadowColor: UIColor?
+    var shadowOpacity: Float?
+    var shadowRadius: CGFloat?
+    var shadowOffset: CGSize?
+    var setShadow: Bool = false
+    
     open var config: CalendarHeatmapConfig! {
         didSet {
             backgroundColor = config.backgroundColor
@@ -34,9 +40,37 @@ open class CalendarHeatmapCell: UICollectionViewCell {
         let path = UIBezierPath(roundedRect: rect, cornerRadius: min(cornerRadius, maxCornerRadius))
         itemColor.setFill()
         path.fill()
-        guard isSelected, config.allowItemSelection else { return }
+        
+        self.updateShadowViews()
+        
+        guard isSelected, config.allowItemSelection else {
+            return
+        }
         config.selectedItemBorderColor.setStroke()
         path.lineWidth = config.selectedItemBorderLineWidth
         path.stroke()
+    }
+    
+    func updateShadowViews() {
+        if self.setShadow == true {
+            return
+        }
+    
+        if self.shadowColor != nil, self.shadowOpacity != nil, self.shadowRadius != nil, self.shadowOffset != nil {
+            self.clipsToBounds = false
+            self.layer.shadowColor = self.shadowColor!.cgColor
+            self.layer.shadowOpacity = self.shadowOpacity!
+            self.layer.shadowRadius = self.shadowRadius!
+            self.layer.shadowOffset = self.shadowOffset!
+            self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: min(config.itemCornerRadius, min(bounds.width, bounds.height) * 0.5)).cgPath
+            self.setShadow = true
+        }
+    }
+    
+    // MARK: Traits
+    
+    open override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        self.setShadow = false
     }
 }
